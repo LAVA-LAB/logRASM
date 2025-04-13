4c13944271df2972b5402e71eae184cf5b177e9c
Wietze Koops <wietzekoops@gmail.com>
2025-04-13 14:53:22 +0200

    correct experiment scripts

--- experiments/run_figures.sh
+++ experiments/run_figures.sh
@@ -15,7 +15,7 @@ models=("LinearSystem --verify_batch_size ${verify_batch_size}"
         "LinearSystem --layout 1 --verify_batch_size ${verify_batch_size}" 
         "MyPendulum --verify_batch_size ${verify_batch_size}" 
         "CollisionAvoidance --noise_partition_cells 24 --verify_batch_size ${collision_batch_size}")
-all_flags="--logger_prefix figures --eps_decrease 0.01 --ppo_max_policy_lipschitz 10 --hidden_layers 3 --expDecr_multiplier 10 --pretrain_method PPO_JAX --pretrain_total_steps 100000 --refine_threshold 250000000"
+all_flags="--logger_prefix figures --eps_decrease 0.01 --ppo_max_policy_lipschitz 10 --hidden_layers 3 --expDecr_multiplier 10 --pretrain_method PPO_JAX --pretrain_total_steps 100000 --refine_threshold 250000000 --forward_pass_batch_size ${forward_pass_batch_size}"
 flags_mesh1="--mesh_loss 0.001 --mesh_loss_decrease_per_iter 0.9"
 flags_mesh2="--mesh_loss 0.01 --mesh_loss_decrease_per_iter 0.8"
 
--- experiments/run_partial_benchmarks.sh
+++ experiments/run_partial_benchmarks.sh
@@ -143,7 +143,7 @@ python table_generator.py --folders sb3
 ### HARD EXPERIMENTS (ONLY TRIPLE INTEGRATOR)
 ############################################################
 
-triple_batch_size = $((verify_batch_size*2/3))
+triple_batch_size=$((verify_batch_size*2/3))
 all_flags="--eps_decrease 0.01 --ppo_max_policy_lipschitz 10 --expDecr_multiplier 10 --pretrain_method PPO_JAX --refine_threshold 250000000 --epochs 100  --forward_pass_batch_size ${forward_pass_batch_size}"
 
 flags_triple="--model TripleIntegrator --logger_prefix TripleIntegrator --pretrain_total_steps 100000 --hidden_layers 3 --mesh_loss 0.005 --mesh_loss_decrease_per_iter 0.9 --mesh_verify_grid_init 0.04 --mesh_verify_grid_min 0.04 --noise_partition_cells 6 --max_refine_factor 4 --verify_batch_size ${triple_batch_size}"
