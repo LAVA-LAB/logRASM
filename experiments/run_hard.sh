91f32c13fb676f131adc8ca7cc458df0bdb8aaff
Thom Badings <thobad@ip-216-45.eduvpn.ru.nl>
2025-05-13 08:45:02 +0200

    run hard experiments for CAV25 camera-ready

--- experiments/run_hard.sh
+++ experiments/run_hard.sh
@@ -16,74 +16,76 @@ all_flags="--eps_decrease 0.01 --ppo_max_policy_lipschitz 10 --expDecr_multiplie
 
 flags_triple="--model TripleIntegrator --logger_prefix TripleIntegrator --pretrain_total_steps 100000 --hidden_layers 3 --mesh_loss 0.005 --mesh_loss_decrease_per_iter 0.9 --mesh_verify_grid_init 0.04 --noise_partition_cells 6 --max_refine_factor 4 --verify_batch_size ${threeD_batch_size}"
 flags_planar="--model PlanarRobot --logger_prefix PlanarRobot --pretrain_total_steps 10000000 --hidden_layers 3 --mesh_loss 0.005 --mesh_loss_decrease_per_iter 0.9 --mesh_verify_grid_init 0.04 --noise_partition_cells 12 --max_refine_factor 4 --verify_batch_size ${threeD_batch_size}"
-flags_drone4D="--model Drone4D --layout 2 --logger_prefix Drone4D --pretrain_total_steps 1000000 --hidden_layers 3 --mesh_loss 0.01 --mesh_verify_grid_init 0.06 --refine_threshold 50000000 --verify_threshold 600000000 --noise_partition_cells 12 --max_refine_factor 2 --verify_batch_size ${verify_batch_size}"
+flags_drone4D="--model Drone4D --layout 2 --logger_prefix Drone4D --pretrain_total_steps 1000000 --hidden_layers 2 --mesh_loss 0.01 --mesh_verify_grid_init 0.06 --refine_threshold 50000000 --verify_threshold 600000000 --noise_partition_cells 12 --max_refine_factor 2 --verify_batch_size ${verify_batch_size}"
+flags_drone4Db="--model Drone4D --layout 2 --logger_prefix Drone4Db --pretrain_total_steps 1000000 --hidden_layers 3 --mesh_loss 0.01 --mesh_verify_grid_init 0.06 --refine_threshold 50000000 --verify_threshold 600000000 --noise_partition_cells 12 --max_refine_factor 2 --verify_batch_size ${verify_batch_size}"
+
 
 # Triple integrator
-#for seed in {1..10};
-#do
-#  for p in 0.8 0.9 0.99 0.9999 0.999999
-#  do
-#    # Our method
-#    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --exp_certificate;
-#    # logRASM only
-#    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --exp_certificate --no-weighted --no-cplip;
-#  done
-#  for p in 0.8 0.9 0.99
-#  do
-#    # Lipschitz only
-#    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --no-exp_certificate;
-#  done
-#done
-##
-#for seed in {1..3};
-#do
-#  for p in 0.9999 0.999999
-#  do
-#    # Lipschitz only
-#    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --no-exp_certificate;
-#  done
-#  for p in 0.8 0.9 0.99 0.9999 0.999999
-#  do
-#    # Baseline
-#    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --no-exp_certificate --no-weighted --no-cplip;
-#done
+for seed in {1..10};
+do
+  for p in 0.8 0.9 0.99 0.999 0.9999 0.99999 0.999999
+  do
+    # Our method
+    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --exp_certificate;
+    # logRASM only
+    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --exp_certificate --no-weighted --no-cplip;
+  done
+  for p in 0.8 0.9 0.99
+  do
+    # Lipschitz only
+    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --no-exp_certificate;
+  done
+done
+#
+for seed in {1..3};
+do
+  for p in 0.999 0.9999 0.99999 0.999999
+  do
+    # Lipschitz only
+    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --no-exp_certificate;
+  done
+  for p in 0.8 0.9 0.99 0.999 0.9999 0.99999 0.999999
+  do
+    # Baseline
+    timeout $TO python run.py --seed $seed $flags_triple $all_flags --probability_bound $p --no-exp_certificate --no-weighted --no-cplip;
+done
 
 # Planar robot
-#for seed in {1..10};
-#do
-#  for p in 0.8 0.9 0.99 0.9999 0.999999
-#  do
-#    # Our method
-#    timeout $TO python run.py --seed $seed $flags_planar $all_flags --probability_bound $p --exp_certificate;
-#  done
-#done
-##
-#for seed in {1..3};
-#do
-#  for p in 0.8 0.9 0.99 0.9999 0.999999
-#  do
-#    # logRASM only
-#    timeout $TO python run.py --seed $seed $flags_planar $all_flags --probability_bound $p --exp_certificate --no-weighted --no-cplip;
-#    # Lipschitz only
-#    timeout $TO python run.py --seed $seed $flags_planar $all_flags --probability_bound $p --no-exp_certificate;
-#    # Baseline
-#    timeout $TO python run.py --seed $seed $flags_planar $all_flags --probability_bound $p --no-exp_certificate --no-weighted --no-cplip;
-#  done
-#done
+for seed in {1..10};
+do
+  for p in 0.8 0.9 0.99 0.999 0.9999 0.99999 0.999999
+  do
+    # Our method
+    timeout $TO python run.py --seed $seed $flags_planar $all_flags --probability_bound $p --exp_certificate;
+  done
+done
+#
+for seed in {1..3};
+do
+  for p in 0.8 0.9 0.99 0.999 0.9999 0.99999 0.999999
+  do
+    # logRASM only
+    timeout $TO python run.py --seed $seed $flags_planar $all_flags --probability_bound $p --exp_certificate --no-weighted --no-cplip;
+    # Lipschitz only
+    timeout $TO python run.py --seed $seed $flags_planar $all_flags --probability_bound $p --no-exp_certificate;
+    # Baseline
+    timeout $TO python run.py --seed $seed $flags_planar $all_flags --probability_bound $p --no-exp_certificate --no-weighted --no-cplip;
+  done
+done
 
 # Drone4D
-for seed in {1..2};
+for seed in {1..10};
 do
-  for p in 0.8 0.9 0.99 0.9999 0.999999
+  for p in 0.8 0.9 0.99 0.999 0.9999 0.99999 0.999999
   do
     # Our method
     timeout $TO python run.py --seed $seed $flags_drone4D $all_flags --probability_bound $p --exp_certificate;
   done
 done
 #
-for seed in {1..2};
+for seed in {1..3};
 do
-  for p in 0.8 0.9 0.99 0.9999 0.999999
+  for p in 0.8 0.9 0.99 0.999 0.9999 0.99999 0.999999
   do
     # logRASM only
     timeout $TO python run.py --seed $seed $flags_drone4D $all_flags --probability_bound $p --exp_certificate --no-weighted --no-cplip;
@@ -91,6 +93,29 @@ do
     timeout $TO python run.py --seed $seed $flags_drone4D $all_flags --probability_bound $p --no-exp_certificate;
     # Baseline
     timeout $TO python run.py --seed $seed $flags_drone4D $all_flags --probability_bound $p --no-exp_certificate --no-weighted --no-cplip;
+  done
+done
+
+# Drone4D (3 layers)
+for seed in {1..10};
+do
+  for p in 0.8 0.9 0.99 0.999 0.9999 0.99999 0.999999
+  do
+    # Our method
+    timeout $TO python run.py --seed $seed $flags_drone4Db $all_flags --probability_bound $p --exp_certificate;
+  done
+done
+#
+for seed in {1..3};
+do
+  for p in 0.8 0.9 0.99 0.999 0.9999 0.99999 0.999999
+  do
+    # logRASM only
+    timeout $TO python run.py --seed $seed $flags_drone4Db $all_flags --probability_bound $p --exp_certificate --no-weighted --no-cplip;
+    # Lipschitz only
+    timeout $TO python run.py --seed $seed $flags_drone4Db $all_flags --probability_bound $p --no-exp_certificate;
+    # Baseline
+    timeout $TO python run.py --seed $seed $flags_drone4Db $all_flags --probability_bound $p --no-exp_certificate --no-weighted --no-cplip;
   done
 done
 
